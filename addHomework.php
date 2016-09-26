8ca86aac106f3b00434b322235d534dfc5d4a9fb
Thaspdev <thaspdev@gmail.com>
2016-09-26 21:24:32 +0200

    Simplified addHomework.php

--- addHomework.php
+++ addHomework.php
@@ -1,7 +1,3 @@
 <?php
-try {
-  $DB = new PDO('mysql:host=localhost;dbname=HWP_DB;charset=utf8','root','');
-} catch (Exception $e) {
-  die('Error : ' . $e->getMessage());
-}
+  include("sharedParts/connectDB.php");
 ?>
