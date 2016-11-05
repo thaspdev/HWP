dc0fab560a47edcf0e7367b63419a6d87a15eed7
thaspdev <thaspdev@gmail.com>
2016-11-05 15:34:35 +0100

    Fixed layout and script error

--- index.php
+++ index.php
@@ -155,6 +155,9 @@
     <?php } ?>
     <script type="text/javascript" src="script.js"></script>
     <script type="text/javascript">
+    var dWClass = ["mo","tu","we","th","fr","sa","su"];
+    var dWAbb = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+    var dWLong = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
     function onres(){
       if (window.innerWidth < 600) {
         for (var i = 0; i<7; i++){
@@ -171,6 +174,33 @@
     }
     window.onload=function(){
       onres();
+    }
+    window.onload=function(){
+      document.getElementById("add").onmouseover=function(){
+        document.getElementById("addSub").style.display = "flex";
+      }
+      document.getElementById("addSub").onmouseover=function(){
+        document.getElementById("addSub").style.display = "flex";
+      }
+      document.getElementById("add").onmouseout=function(){
+        document.getElementById("addSub").style.display = "none";
+      }
+      document.getElementById("addSub").onmouseout=function(){
+        document.getElementById("addSub").style.display = "none";
+      }
+      //USER & USERSUB
+      document.getElementById("user").onmouseover=function(){
+        document.getElementById("userSub").style.display = "flex";
+      }
+      document.getElementById("userSub").onmouseover=function(){
+        document.getElementById("userSub").style.display = "flex";
+      }
+      document.getElementById("user").onmouseout=function(){
+        document.getElementById("userSub").style.display = "none";
+      }
+      document.getElementById("userSub").onmouseout=function(){
+        document.getElementById("userSub").style.display = "none";
+      }
     }
     </script>
   </body>
--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-var dWClass = ["mo","tu","we","th","fr","sa","su"];
-var dWAbb = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
-var dWLong = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 window.onload=function(){
   document.getElementById("add").onmouseover=function(){
     document.getElementById("addSub").style.display = "flex";
