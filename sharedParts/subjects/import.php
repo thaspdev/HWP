d3ab1471c51f0ebca36c493ab4801b8287fc3b17
thaspdev <thaspdev@gmail.com>
2016-11-10 22:24:12 +0100

    Secured import for subjects & types

--- sharedParts/subjects/import.php
+++ sharedParts/subjects/import.php
@@ -1,7 +1,11 @@
 <?php
+  include_once('sharedParts/groups/import.php');
+  $Groups__subjects = $GUArray;
+  unset($Groups__subjects[0]);
+  $GSstr__ = join("', '",$Groups__subjects);
   $SubArrayNames = array(0 => "No subject");
-  $SubAnswer = $DB->prepare('SELECT * FROM subjects');
-  $SubAnswer->execute();
+  $SubAnswer = $DB->prepare('SELECT * FROM subjects WHERE userID = ? OR groupID IN ?');
+  $SubAnswer->execute(array($_SESSION['userID'], $GSstr__));
   while($SubData = $SubAnswer->fetch()) {
     $SubArrayNames[$SubData['subjectID']] = $SubData['subjectName'];
   }
--- sharedParts/types/import.php
+++ sharedParts/types/import.php
@@ -1,4 +1,5 @@
 <?php
+  include_once('sharedParts/groups/import.php');
   if(isset($_SESSION['userID'])) {
     $TypeArrayNames = array(
       0 => "Generic",
@@ -11,8 +12,11 @@
       -2 => 30,
       -3 => 60,
     );
-    $TypeAnswer = $DB->prepare('SELECT * FROM types');
-    $TypeAnswer->execute();
+    $Groups__types = $GUArray;
+    unset($Groups__types[0]);
+    $GTstr__ = join("', '",$Groups__types);
+    $TypeAnswer = $DB->prepare('SELECT * FROM types WHERE userID = ? OR groupID IN ?');
+    $TypeAnswer->execute(array($_SESSION['userID'], $GTstr));
     while($TypeData = $TypeAnswer->fetch()) {
       $TypeArrayNames[$TypeData['typeID']] = $TypeData['typeName'];
       $TypeArrayDurations[$TypeData['typeID']] = $TypeData['estimatedDuration'];
