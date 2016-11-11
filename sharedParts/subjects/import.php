1b7c36f88884ee39d207da85726acb241f28ca5a
thaspdev <thaspdev@gmail.com>
2016-11-11 18:55:41 +0100

    Fixed disconnected user errors

--- sharedParts/subjects/import.php
+++ sharedParts/subjects/import.php
@@ -1,12 +1,14 @@
 <?php
-  include_once('sharedParts/groups/import.php');
-  $Groups__subjects = $GUArray;
-  unset($Groups__subjects[0]);
-  $GSstr__ = implode(",",$Groups__subjects);
-  $SubArrayNames = array(0 => "No subject");
-  $SubAnswer = $DB->prepare('SELECT * FROM subjects WHERE userID = ? OR groupID IN (?)');
-  $SubAnswer->execute(array($_SESSION['userID'], $GSstr__));
-  while($SubData = $SubAnswer->fetch()) {
-    $SubArrayNames[$SubData['subjectID']] = $SubData['subjectName'];
+  if(isset($_SESSION['userID'])) {
+    include_once('sharedParts/groups/import.php');
+    $Groups__subjects = $GUArray;
+    unset($Groups__subjects[0]);
+    $GSstr__ = implode(",",$Groups__subjects);
+    $SubArrayNames = array(0 => "No subject");
+    $SubAnswer = $DB->prepare('SELECT * FROM subjects WHERE userID = ? OR groupID IN (?)');
+    $SubAnswer->execute(array($_SESSION['userID'], $GSstr__));
+    while($SubData = $SubAnswer->fetch()) {
+      $SubArrayNames[$SubData['subjectID']] = $SubData['subjectName'];
+    }
   }
 ?>
