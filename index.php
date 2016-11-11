5984598fc5877e338f3c7a0f2af8b9b1bdc22aaf
thaspdev <thaspdev@gmail.com>
2016-11-11 09:32:03 +0100

    Secured HW imoprt in index.php

--- index.php
+++ index.php
@@ -37,8 +37,11 @@
           <?php
           /*QUERY*/
           if (isset($_SESSION['userID'])) {
-            $HWListAnswer = $DB->prepare('SELECT * FROM homeworkList WHERE (userID = ? OR groupID = ?) ORDER BY deadline');
-            $HWListAnswer->execute(array(htmlspecialchars($_SESSION['userID']), $_SESSION['groupID']));
+            $GArrayHW__ = $GUArray;
+            unset($GArrayHW__[0]);
+            $GstrHW__ = join("', '",$GArrayHW);
+            $HWListAnswer = $DB->prepare('SELECT * FROM homeworkList WHERE (userID = ? OR groupID IN ?) ORDER BY deadline');
+            $HWListAnswer->execute(array(htmlspecialchars($_SESSION['userID']), $GstrHW__));
             $totalWork = 0;
             while ($HWListData = $HWListAnswer->fetch())
             {
