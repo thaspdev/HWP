192a6325a23ad944a4ff5617b725f816f0ad7b7a
thaspdev <thaspdev@gmail.com>
2016-11-11 09:31:18 +0100

    Secured NU teachers import

--- sharedParts/teachers/import.php
+++ sharedParts/teachers/import.php
@@ -1,5 +1,6 @@
 <?php
 //Teachers
+  include_once('sharedParts/groups/import.php');
   if(isset($_SESSION['userID'])){
     $TArrayNames = array(0 => "No teacher");
     $TArrayIDs = array(0 => 0);
@@ -9,8 +10,11 @@
       $TArrayNames[$TData['userID']] = $TData['firstName'] . " " . $TData['lastName'];
       $TArrayIDs[$TData['firstName'] . " " . $TData['lastName']] = $TData['userID'];
     }
-    $NUTeacherAnswer = $DB->prepare('SELECT teacherID, firstName, lastName FROM nonUserTeachers');
-    $NUTeacherAnswer->execute();
+    $Groups__NUT = $GUArray;
+    unset($Groups__NUT[0]);
+    $GNUTstr__ = join("', '",$Groups__NUT);
+    $NUTeacherAnswer = $DB->prepare('SELECT teacherID, firstName, lastName FROM nonUserTeachers WHERE userID = ? OR groupID IN ?');
+    $NUTeacherAnswer->execute(array($_SESSION['userID'], $GNUTstr__));
     while($NUTData = $NUTeacherAnswer->fetch()) {
       $TArrayNames[(-1)*$NUTData['teacherID']] = $NUTData['firstName'] . " " . $NUTData['lastName'];
       $TArrayIDs[$NUTData['firstName'] . " " . $NUTData['lastName']] = (-1)*$NUTData['teacherID'];
